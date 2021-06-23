30b26453409fba98b1d8ae78c76befc74b8649a6
Sandro Bormolini <86348794+sbormolini@users.noreply.github.com>
2021-06-23 14:48:15 +0200

    Update api_scriptblock.ps1

--- TestPolaris/api_scriptblock.ps1
+++ TestPolaris/api_scriptblock.ps1
@@ -5,14 +5,14 @@
 #>
 
 #Import-Module "$($PSScriptRoot)\TestClass\TestClass.psm1" -Force
-Import-Module "C:\Users\sandr\OneDrive - itnetX AG\Customers\Swisscom\Polaris\TestClass\TestClass.psm1" -Force
+Import-Module ".\Polaris\TestClass\TestClass.psm1" -Force
 
 $list = New-Object System.Collections.Generic.List[Object]
 
 # add user to list 
 #$list.Add([TestUser]::New("Hans Peter",23))
 $list.Add((New-TestUser -Name "Jenny" -Age 44))
-$list.Add((New-TestUser -Name "Caro" -Age 44))
+$list.Add((New-TestUser -Name "Lia" -Age 36))
 $json = ConvertTo-Json -InputObject $list
 
 return $Response.Json($json)
